645ff7744a66fe988c7a6d7f0b6577a697cbc949
ricky52194 <ramosrs@g.cofc.edu>
2018-01-16 02:49:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ricky52194/github_workflow/MainActivity_HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.ricky52194.github_workflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__hello_world);
+    }
+}
